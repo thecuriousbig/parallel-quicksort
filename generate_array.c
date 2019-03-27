4588c0b8896dfdaebdc0f239da5158f46a51d552
TanatornBig <tanatorn.nateesanprasert@gmail.com>
2019-03-27 22:20:57 +0700

    fix generate_array to make it able to random floating point number between 0 to upper bound

--- generate_array.c
+++ generate_array.c
@@ -6,17 +6,15 @@ int main(int argc, char **argv)
   FILE *fp;
   char *ptr;
   int i;
-  int randomNumber;
+  float randomNumber;
 
   /* size of array */
   int size = strtol(argv[1], &ptr, 10);
-  /* lower bound of array's random value */
-  int lower = strtol(argv[2], &ptr, 10);
   /* upper bound of array's random value */
-  int upper = strtol(argv[3], &ptr, 10);
+  int upper = strtol(argv[2], &ptr, 10);
 
   printf("Generate random number array size (%d x 1)\n", size);
-  printf("Range of array's value is between %d - %d\n", lower, upper);
+  printf("Range of array's value is between 0 - %d\n", upper);
 
   printf("Begin write file array.txt ..\n");
 
@@ -29,12 +27,16 @@ int main(int argc, char **argv)
 
   /* Write size of array in 1st line */
   fprintf(fp, "%d\n", size);
+
+  /* make random unique */
+  srand((unsigned int)time(NULL));
+
   /* Write rest of array to the file */
   for (i = 0; i < size; i++)
   {
     /* random number between the range upper - lower */
-    randomNumber = (rand() % (upper - lower + 1)) + lower;
-    fprintf(fp, "%d\n", randomNumber);
+    randomNumber = ((float)rand() / (float)RAND_MAX) * upper;
+    fprintf(fp, "%.1f\n", randomNumber);
   }
 
   /* close file */
